42010913b4aa4119da196bdca48c39c4d1cbb440
skfkgla <narahim.lee@gmail.com>
2022-06-29 11:07:28 +0900

    starter files

---
+++ src/main/java/com/Backend_Homework2/BackendHomework2Application.java
@@ -0,0 +1,13 @@
+package com.Backend_Homework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHomework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHomework2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Backend_Homework2/BackendHomework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend_Homework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHomework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
